433a17608849bc597ca48f8b204d1e7772f6d649
NisagiPathirana <kpndomalsa19@gmail.com>
2025-11-15 13:25:41 +0530

    create Schedule dto class

---
+++ src/main/java/edu/icet/model/dto/Schedule.java
@@ -0,0 +1,18 @@
+package edu.icet.model.dto;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Schedule {
+
+    private String scheduleId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String timeDuration;
+}
